b51a5676222d95c2a9ad75d43e6b31b6fdb36cde
Islam Mušić <islam.music@scv.si>
2023-04-14 10:27:58 +0200

    User crud

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,19 +5,23 @@ import { UserModule } from './user/user.module';
 import { DatabaseModule } from './database/database.module';
 import {TypeOrmModule} from "@nestjs/typeorm";
 import {ConfigModule} from "@nestjs/config";
+import * as process from "process";
 
 
 @Module({
   imports: [
+      ConfigModule.forRoot({
+          isGlobal: true
+      }),
       UserModule,
       DatabaseModule,
       TypeOrmModule.forRoot({
         type: 'postgres',
-        host: 'localhost',
-        port: 5432,
-        username: 'postgres',
-        password: 'postgres',
-        database: 'node23',
+        host: process.env.DATABASE_HOST || 'localhost',
+        port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
+        username: process.env.DATABASE_USERNAME,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE_NAME,
           autoLoadEntities: true,
           entities: [ ],
           synchronize: true
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import {User} from "../entities/user.entity";
 import {TypeOrmModule} from "@nestjs/typeorm";
+import { UserService } from './user.service';
 
 
 @Module({
   imports: [
       TypeOrmModule.forFeature([User])
   ],
-  controllers: [UserController]
+  controllers: [UserController],
+  providers: [UserService]
 })
 export class UserModule {}
